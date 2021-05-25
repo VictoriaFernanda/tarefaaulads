017b60fe0a1184b8cba51af6e0d3c9eaec89aa64
VictoriaFernanda <84817181+VictoriaFernanda@users.noreply.github.com>
2021-05-25 20:02:35 -0300

    Create base.cs

---
+++ base.cs
@@ -0,0 +1,35 @@
+using  System ;
+usando o  sistema . Coleções . Genérico ;
+usando o  sistema . Texto ;
+
+namespace  saladeaula
+{
+  classe  pública base_aluno
+ {
+         nome da string ;
+         faltas duplas ;
+         notas duplas ;
+
+    public  string  Nome
+    {
+            get { return  nome ; }
+            definir { nome  =  valor ; }
+
+
+    }
+
+         faltas duplas  públicas
+        {
+            get { return  faltas ; }
+            definir { faltas  =  valor ; }
+
+        }
+
+         notas duplas  públicas
+        {
+            get { return  notas ; }
+            definir { faltas  =  valor ; }
+
+        }
+    } 
+}
